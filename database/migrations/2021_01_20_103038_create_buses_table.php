8b6a6d2c734f5b843412960bd2dfcf81251c7bf9
quyle92 <free2idol@gmail.com>
2021-01-20 17:47:27 +0700

    init

---
+++ database/migrations/2021_01_20_103038_create_buses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('bus_name');
+            $table->string('bus_code');
+            $table->string('operator_id');
+            $table->string('total_seats');
+            $table->string('status')->default(1);
+                
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buses');
+    }
+}
